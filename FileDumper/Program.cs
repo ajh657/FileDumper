19ab1871050d988b6b0cb1b34a6535c0fbf4c5dc
ajh657 <33629266+ajh657@users.noreply.github.com>
2019-06-02 20:40:28 +0300

    Completed

---
+++ FileDumper/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileDumper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo directory = new DirectoryInfo(@"" + Console.ReadLine());
+            DirectoryInfo output = new DirectoryInfo(@"" + Console.ReadLine());
+            FileInfo[] Files = directory.GetFiles();
+
+            Console.WriteLine();
+
+            foreach (FileInfo item in Files)
+            {
+                Console.WriteLine("Dumping file: " + item.Name);
+                Console.WriteLine();
+                Byte[] toDump = File.ReadAllBytes(item.FullName);
+                string byteText = Convert.ToBase64String(toDump);
+
+                Console.WriteLine("Writing File: " + output.FullName + "/" + item.FullName + ".txt");
+                File.WriteAllText(output.FullName + "\\" + item.Name + ".txt", byteText);
+            }
+        }
+    }
+}
